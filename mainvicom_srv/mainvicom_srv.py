df46200b6753344c636300f4fae1702fb7cc5a70
cropinghigh indir <joinmark60@gmail.com>
2024-12-28 02:14:38 +0300

    autopendel system

--- mainvicom_srv/mainvicom_srv.py
+++ mainvicom_srv/mainvicom_srv.py
@@ -22,6 +22,7 @@ mainvicom_interface = Domru_Icom_Interface()
 ffmpeg_v_proc = -1
 ffmpeg_a_proc = -1
 local_a_proc = -1
+ffmpeg_v_proc_dest = ""
 
 class UdpHandler(asyncio.DatagramProtocol):
     def __init__(self, loop, mvc_ifc) -> None:
@@ -95,10 +96,12 @@ def stop_ffmpeg_a_proc():
 
 def start_ffmpeg_av_proc(source, dest):
     global ffmpeg_v_proc
+    global ffmpeg_v_proc_dest
     if(ffmpeg_v_proc == -1):
         ffmpeg_v_proc = []
-        ffmpeg_v_proc_a = subprocess.Popen(["bash","-c","ffmpeg -re -probesize 65536 -analyzeduration 65536 -fflags nobuffer -flags low_delay -i " + source + " -r 6 -vcodec mpeg2video -filter:v \"scale=640x480\" -bufsize 128k -maxrate 4000k -qscale:v 2 -fflags nobuffer -flags low_delay -avioflags direct -an -f mpegts udp://" + dest + ":8101?pkt_size=1024 -vn -filter:a \"volume=" + volume_downlink + "\" -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8105?pkg_size=1024"], preexec_fn=os.setsid)
-        ffmpeg_v_proc_b = subprocess.Popen(["bash","-c","ffmpeg -re -probesize 32768 -analyzeduration 32768 -fflags nobuffer -flags low_delay -f s16le -ac 1 -ar 8000 -i udp://127.0.0.1:8103?listen=1 -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://" + dest + ":8104?pkt_size=1024"], preexec_fn=os.setsid)
+        ffmpeg_v_proc_dest = dest
+        ffmpeg_v_proc_a = subprocess.Popen(["bash","-c","ffmpeg -hide_banner -loglevel error -re -probesize 65536 -analyzeduration 65536 -fflags nobuffer -flags low_delay -i " + source + " -r 6 -vcodec mpeg2video -filter:v \"scale=640x480\" -bufsize 128k -maxrate 4000k -qscale:v 2 -fflags nobuffer -flags low_delay -avioflags direct -an -f mpegts udp://" + dest + ":8101?pkt_size=1024 -vn -filter:a \"volume=" + volume_downlink + "\" -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8105?pkg_size=1024"], preexec_fn=os.setsid)
+        ffmpeg_v_proc_b = subprocess.Popen(["bash","-c","ffmpeg -hide_banner -loglevel error -re -probesize 65536 -analyzeduration 65536 -fflags nobuffer -flags low_delay -f s16le -ac 1 -ar 8000 -i udp://127.0.0.1:8103?listen=1 -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://" + dest + ":8104?pkt_size=1024"], preexec_fn=os.setsid)
         ffmpeg_v_proc = [ffmpeg_v_proc_a, ffmpeg_v_proc_b]
         print("Started v ffmpeg")
 
@@ -106,14 +109,14 @@ def start_ffmpeg_a_proc_fromvideo(source, dest):
     global ffmpeg_a_proc
     if(ffmpeg_a_proc == -1):
         ffmpeg_a_proc = 1
-        ffmpeg_a_proc = subprocess.Popen(["bash","-c","ffmpeg -re -probesize 32768 -analyzeduration 32768 -fflags nobuffer -flags low_delay -f s16le -ac 1 -ar 8000 -i udp://127.0.0.1:8105?listen=1 -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8103?pkt_size=1024"], preexec_fn=os.setsid)
+        ffmpeg_a_proc = subprocess.Popen(["bash","-c","ffmpeg -hide_banner -loglevel error -re  -probesize 65536 -analyzeduration 65536 -fflags nobuffer -flags low_delay -f s16le -ac 1 -ar 8000 -i udp://127.0.0.1:8105?listen=1 -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8103?pkt_size=1024"], preexec_fn=os.setsid)
         print("Started a fromvideo ffmpeg")
 
 def start_ffmpeg_a_proc_fromfile(source, dest):
     global ffmpeg_a_proc
     if(ffmpeg_a_proc == -1):
         ffmpeg_a_proc = 1
-        ffmpeg_a_proc = subprocess.Popen(["bash","-c","ffmpeg -re -stream_loop -1 -i " + source + " -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8103?pkt_size=1024"], preexec_fn=os.setsid)
+        ffmpeg_a_proc = subprocess.Popen(["bash","-c","ffmpeg -hide_banner -loglevel error -re -stream_loop -1 -i " + source + " -fflags nobuffer -flags low_delay -avioflags direct -acodec pcm_s16le -ac 1 -ar 8000 -f s16le udp://127.0.0.1:8103?pkt_size=1024"], preexec_fn=os.setsid)
         print("Started a fromfile ffmpeg")
 
 async def start_local_a_proc(source, dest):
@@ -136,6 +139,47 @@ async def has_data(reader: asyncio.StreamReader) -> bool:
         return True
     except asyncio.TimeoutError:
         return False
+
+async def autopendel_ffmpeg():
+    global ffmpeg_v_proc
+    global ffmpeg_a_proc
+    print("FFMPEG Autopendel started")
+    while(1):
+        if(ffmpeg_v_proc != -1):
+            fall = False
+            for proc in ffmpeg_v_proc:
+                if(proc.poll() != None):
+                    print("FFMPEG V PROC FALLED! RESTARTING...")
+                    fall = True
+            if(fall):
+                for proc in ffmpeg_v_proc:
+                    proc.poll()
+                    if(proc.returncode == None):
+                        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+                        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+                        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+                        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+                        time.sleep(0.01)
+                        os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+                print("Killed ffmpeg v proc")
+
+                r = mainvicom_interface.stream_request()
+                if(r != -1):
+                    ffmpeg_v_proc = -1
+                    start_ffmpeg_av_proc(r, ffmpeg_v_proc_dest)
+                # print("Retarting ffmpeg v procs with args: " + str(ffmpeg_v_proc[0].args) + " ; " + str(ffmpeg_v_proc[1].args))
+                # ffmpeg_v_proc_a = subprocess.Popen(ffmpeg_v_proc[0].args, preexec_fn=os.setsid)
+                # ffmpeg_v_proc_b = subprocess.Popen(ffmpeg_v_proc[1].args, preexec_fn=os.setsid)
+                # ffmpeg_v_proc = [ffmpeg_v_proc_a, ffmpeg_v_proc_b]
+                # print("Restarted v ffmpeg")
+        if(ffmpeg_a_proc != -1):
+            if(ffmpeg_a_proc.poll() != None):
+                print("FFMPEG A PROC FALLED! RESTARTING...")
+
+                print("Retarting ffmpeg a proc with args: " + str(ffmpeg_a_proc.args))
+                ffmpeg_a_proc = subprocess.Popen(ffmpeg_a_proc.args, preexec_fn=os.setsid)
+                print("Restarted a ffmpeg")
+        await asyncio.sleep(0.5)
 
 async def handle_client(reader, writer):
     # Get client's address
@@ -271,11 +315,16 @@ async def main():
         handle_client, '0.0.0.0', 8100
     )
 
+    task = asyncio.create_task(autopendel_ffmpeg())
+
     addr = server.sockets[0].getsockname()
     print(f'Listening on {addr}')
 
     async with server:
         await server.serve_forever()
+    
+    stop_proc()
+    await task
 
 async def intr():
     print("CTRL+C CAUGHT! STOPPING!")
